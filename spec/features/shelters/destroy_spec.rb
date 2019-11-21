5da5c64b7c447e0deaf87ecd298fb9aa51c8eb67
Becky Robran <rerobran@gmail.com>
2019-11-21 15:44:45 -0700

    updates name of test shelter

--- spec/features/shelters/destroy_spec.rb
+++ spec/features/shelters/destroy_spec.rb
@@ -4,7 +4,7 @@ RSpec.describe "as a vistior", type: :feature do
   describe "when I visit a shelter show page" do
     it "I can click on a link to delete a shelter and redirect back to shelter index " do
 
-      shelter_1 = Shelter.create(name: "Best Shelter Denver",
+      shelter_1 = Shelter.create(name: "Best Shelter Cincinatti",
                                  address: "323 Boulder St",
                                  city: "Denver",
                                  state: "CO",
@@ -22,9 +22,8 @@ RSpec.describe "as a vistior", type: :feature do
       click_link 'Delete Shelter'
 
       expect(current_path).to eq("/shelters")
-
       expect(page).to have_content(shelter_2.name)
-      #expect(page).to_not have_content(shelter_1.name)
+      expect(page).to_not have_content(shelter_1.name)
     end
   end
 end
