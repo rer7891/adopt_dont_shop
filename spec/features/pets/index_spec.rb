6bc8a1ffdd165483c3af85542c2d980d4e344288
Becky Robran <rerobran@gmail.com>
2019-11-22 12:04:53 -0700

    Updates testing for pets index page

--- spec/features/pets/index_spec.rb
+++ spec/features/pets/index_spec.rb
@@ -1,12 +1,22 @@
 require 'rails_helper'
 
-RSpec.describe "As a visitor", :type :feature do
+RSpec.describe "As a visitor", type: :feature do
   describe "when I visit /pets" do
     it "can see each pet in the system" do
-      pet_1 = Pet.create(name: "Fluffy",
+      shelter = Shelter.create(name: "Best Shelter Broomfield",
+                                 address: "124 Main Street",
+                                 city: "Broomfield",
+                                 state: "CO",
+                                 zip: 80020)
+
+      pet = Pet.create(image:"https://placekitten.com/200/300",
+                         name: "Fluffy",
                          age: 9,
                          sex: "female",
-                         shelter_name: shelter_id.name)
+                         shelter: shelter)
+    visit '/pets'
+
+    expect(page).to have_content(pet.name)
 
     end
   end
