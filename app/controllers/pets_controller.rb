5445a695b8e474e6570b2e5061f5c2cbb1af9f03
Becky Robran <rerobran@gmail.com>
2019-11-23 10:29:13 -0700

    "Adds show path"

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -7,4 +7,8 @@ class PetsController < ApplicationController
   def find
     @pet_by_id = Pet.where(shelter_id: params[:shelter_id]).order(created_at: :desc)
   end
+
+  def show
+    @pets = Pet.find(params[:pet_id])
+  end 
 end
