aff809561bfbbc605b042b0ee3d6fd0a2b36308b
Becky Robran <rerobran@gmail.com>
2019-11-23 18:29:35 -0700

    Adds update a pet testing

---
+++ spec/features/pets/update_spec.rb
@@ -0,0 +1,34 @@
+require 'rails_helper'
+
+RSpec.describe "As a visitor", type: :feature do
+  describe "when I visit a pet show page" do
+    it "I can click a link and update that pet" do
+      shelter = Shelter.create(name: "Best Shelter Broomfield",
+                                 address: "124 Main Street",
+                                 city: "Broomfield",
+                                 state: "CO",
+                                 zip: 80020)
+
+      pet = Pet.create(image:"https://placekitten.com/200/300",
+                         name: "Fluffy",
+                         description: "Fluffy is a cutie.",
+                         age: 9,
+                         sex: "female",
+                         shelter: shelter)
+
+      visit "/pets/#{pet.id}"
+
+      click_link "Update Pet Info"
+
+      expect(current_path).to eq("/pets/#{pet.id}/edit")
+      fill_in 'Name', with: "Felix"
+      fill_in 'Description', with: "Felix is wonderful."
+      fill_in 'Age', with: 6
+      fill_in 'Sex', with: "Female"
+      click_on 'Update Pet Details'
+
+      expect(current_path).to eq("/pets/#{pet.id}")
+      expect(page).to have_content("Felix")
+    end
+  end
+end
