36a87bb08d0090f0a36e7daced655b533f1f90fb
Becky Robran <rerobran@gmail.com>
2019-11-24 20:21:16 -0700

    Updates testing with a before each block

--- spec/features/shelters/index_spec.rb
+++ spec/features/shelters/index_spec.rb
@@ -1,41 +1,47 @@
 require 'rails_helper'
 
 RSpec.describe "shelters index page", type: :feature do
-  it "can see all shelters names" do
-    shelter_1 = Shelter.create(name: "Best Shelter Denver",
+  before :each do
+    @shelter_1 = Shelter.create(name: "Best Shelter Denver",
                                address: "134 Main St",
                                city: "Denver",
                                state: "CO",
                                zip: 82420)
 
-    shelter_2 = Shelter.create(name: "Boulder Shelter Adopt",
+    @shelter_2 = Shelter.create(name: "Boulder Shelter Adopt",
                                address: "232 Floyd Way",
                                city: "Boulder",
                                state: "CO",
                                zip: 80220)
+  end
+  it "can see all shelters names" do
 
     visit "/shelters"
 
-    expect(page).to have_content(shelter_1.name)
-    expect(page).to have_content(shelter_2.name)
+    expect(page).to have_content(@shelter_1.name)
+    expect(page).to have_content(@shelter_2.name)
   end
 
   it "has link names to a show page" do
-    shelter_1 = Shelter.create(name: "Best Shelter Denver",
-                               address: "134 Main St",
-                               city: "Denver",
-                               state: "CO",
-                               zip: 82420)
-
-    shelter_2 = Shelter.create(name: "Boulder Shelter Adopt",
-                               address: "232 Floyd Way",
-                               city: "Boulder",
-                               state: "CO",
-                               zip: 80220)
 
     visit "/shelters"
     expect(page).to have_link("Best Shelter Denver")
     expect(page).to have_link("Boulder Shelter Adopt")
+  end
+
+  it "has a link to edit or delete each shelter" do
+
+    visit "/shelters"
+    find("a[href='/shelters/#{@shelter_1.id}/edit']").click
+    expect(current_path).to eql("/shelters/#{@shelter_1.id}/edit")
+    visit "/shelters"
+
+    within("li##{@shelter_1.id}") do
+      click_link "Delete Shelter"
+      expect(current_path).to eql("/shelters")
+    end
+    expect(page).to_not have_content(@shelter_1.name)
+
   end
 
 end
